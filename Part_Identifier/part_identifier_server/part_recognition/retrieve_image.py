10ffcc4a6472fa5f7544e3bcb321a46ac322dd0c
Davit Barblishvili <db3230@columbia.edu>
2022-04-04 11:45:15 -0400

    Update retrieve_image.py

--- Part_Identifier/part_identifier_server/part_recognition/retrieve_image.py
+++ Part_Identifier/part_identifier_server/part_recognition/retrieve_image.py
@@ -16,21 +16,21 @@ for feature_path in Path("./parts_features").glob("*.npy"):
     img_paths.append(Path("./rendered_parts") / (feature_path.stem + ".png"))
 features = np.array(features)
 
-img = Image.open("./parts_test_data/1.png")
+img = Image.open("./parts_test_data/6.png")
 # Extract its features
 query = fe.extract(img)
 # Calculate the similarity (distance) between images
 dists = np.linalg.norm(features - query, axis=1)
 # Extract 5 images that have lowest distance
-ids = np.argsort(dists)[:5]
+ids = np.argsort(dists)[:4]
 scores = [(dists[id], img_paths[id]) for id in ids]
 # Visualize the result
 axes=[]
-fig=plt.figure(figsize=(8,8))
+fig=plt.figure(figsize=(14,7))
 
-for a in range(1*5):
+for a in range(1*4):
     score = scores[a]
-    axes.append(fig.add_subplot(1, 5, a+1))
+    axes.append(fig.add_subplot(1, 4, a+1))
     subplot_title=str(score[0])
     axes[-1].set_title(subplot_title)  
     plt.axis('off')
